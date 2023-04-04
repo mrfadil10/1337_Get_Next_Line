7a55e0ff105c6152e53398ef555c37020bdf1f48
Liver <mohamedfadel2077@gmail.com>
2023-04-04 02:09:03 +0000

    finished

---
+++ get_next_line_bonus.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mfadil <mfadil@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/03 02:26:46 by mfadil            #+#    #+#             */
+/*   Updated: 2023/04/04 02:05:40 by mfadil           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+char	*read_fd(int fd, char *str)
+{
+	char	*buf;
+	int		read_bytes;
+
+	buf = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!buf)
+		return (NULL);
+	read_bytes = 1;
+	while (!ft_strchr(str, '\n') && read_bytes != 0)
+	{
+		read_bytes = read(fd, buf, BUFFER_SIZE);
+		if (read_bytes == -1)
+		{
+			free(buf);
+			return (NULL);
+		}
+		buf[read_bytes] = '\0';
+		str = ft_strjoin(str, buf);
+	}
+	free(buf);
+	return (str);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*str[OPEN_MAX];
+	char		*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, NULL, 0) == -1)
+	{
+		if (str[fd])
+		{
+			free(str[fd]);
+			str[fd] = NULL;
+		}
+		return (NULL);
+	}
+	str[fd] = read_fd(fd, str[fd]);
+	if (!str[fd])
+		return (NULL);
+	line = get_lines(str[fd]);
+	str[fd] = ft_new_left_str(str[fd]);
+	return (line);
+}
---
+++ get_next_line_bonus.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mfadil <mfadil@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/03 02:27:22 by mfadil            #+#    #+#             */
+/*   Updated: 2023/04/03 21:32:14 by mfadil           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <limits.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+size_t	ft_strlen(const char *str);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strchr(char *s, int c);
+char	*get_next_line(int fd);
+char	*get_lines(char *left_str);
+char	*ft_new_left_str(char *left_str);
+char	*get_next_line(int fd);
+
+#endif
